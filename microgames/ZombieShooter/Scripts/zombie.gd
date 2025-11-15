6a8c31f12556601ce0d7d5223eafc286356f0b43
Aidan Heathfield <aidan.heathfield@gmail.com>
2025-11-15 02:19:30 -0500

    Learned how to add animations separate from the model, used this to add an attack animation, used animation tree so that the zombie attacks player in range

--- microgames/ZombieShooter/Scripts/zombie.gd
+++ microgames/ZombieShooter/Scripts/zombie.gd
@@ -2,8 +2,10 @@ extends CharacterBody3D
 
 @export var player : CharacterBody3D
 @export var speed : float = 5.0
+@export var attack_range : float = 2.5
 
 @onready var nav_agent = $NavigationAgent3D
+@onready var animation_tree = $AnimationTree
 
 # Physics process is better for things that have physics aka collision since it relies on a set physics framerate (i.e. 60fps)
 func _physics_process(_delta) -> void:
@@ -18,4 +20,11 @@ func _physics_process(_delta) -> void:
 	# keeping the y rotation same as zombie since it may mess up if looking at player
 	look_at(Vector3(player.global_position.x, global_position.y, player.global_position.z), Vector3.UP)
 	
+	# Animation Conditions
+	animation_tree.set("parameters/conditions/attack", _player_in_range())
+	
 	move_and_slide()
+
+
+func _player_in_range() -> bool:
+	return global_position.distance_to(player.global_position) <= attack_range
